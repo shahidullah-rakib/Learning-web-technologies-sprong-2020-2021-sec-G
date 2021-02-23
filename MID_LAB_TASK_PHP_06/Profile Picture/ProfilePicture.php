1afa82a7aa922388d02517187d0f5af22cf84aee
shsidulla <57812923+shsidulla@users.noreply.github.com>
2021-02-23 09:18:15 +0600

    3rd commit(profile picture)

---
+++ MID_LAB_TASK_PHP_06/Profile Picture/ProfilePicture.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lohin Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>PROFILE PICTURE</legend>
+            <img src="Capture.jpg" alt=""><br>
+            <input type="file" id="img" name="img" accept="image/*">
+            _______________________________________________________________ <br>
+            <input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body> 
+</html>
