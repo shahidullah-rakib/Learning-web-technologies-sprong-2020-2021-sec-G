4181f9301bfeb8bb977a131b25458ff0509b14de
shsidulla <57812923+shsidulla@users.noreply.github.com>
2021-02-25 23:06:18 +0600

    Add files via upload

---
+++ MID_LAB_TASK_PHP_SESSION_07/logincheck.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+
+    if(isset($_POST['submit']))
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if($username == "" || $password =="")
+        {
+            echo "null value found......";
+        }
+        else
+        {
+            if($_SESSION['username'] == $username &&  $_SESSION['password'] == $password)
+            {
+                $_SESSION['flag'] = true;
+                //echo "valied user...";
+                header('location: home.php');
+            }
+            else
+            {
+                echo "incorrect user name or pass";
+            }
+        }
+    }
+    
+
+?>
