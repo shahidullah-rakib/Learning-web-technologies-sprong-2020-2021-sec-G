cb9d02a4f2fa2fc42700a2fa41835de9d7702cfa
shsidulla <57812923+shsidulla@users.noreply.github.com>
2021-03-14 10:31:03 +0600

    First commit at 10:30 AM

---
+++ LAB-EXAM_set-A/view/registration.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <table border="0" width="100%">
+        <tr>
+            <td></td>
+            <td  height="600px" align="center">
+                <form method="POST" action="" enctype="multipart/form-data">
+                    <fieldset>
+                        <legend><b>REGISTRATION</b></legend>
+                        <table border="0">
+                            <tr>
+                                <td>ID</td>
+                                <td>:
+                                    <input type="text" name="id" id="">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><hr></td>
+                            </tr>
+                            <tr>
+                                <td>Password</td>
+                                <td>:
+                                    <input type="password" name="pass" id="">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><hr></td>
+                            </tr>
+                            <tr>
+                                <td>Confirm Password</td>
+                                <td>:
+                                    <input type="password" name="cPass" id="">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><hr></td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td>:
+                                    <input type="text" name="name" id="">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><hr></td>
+                            </tr>
+                        
+                            
+                            <tr>
+                                <td colspan="2">
+                                    <input type="submit" name="submit" value="Submit">
+                                    <input type="reset" value="reset">
+                                </td>
+                            </tr>
+                        </table>
+                    </fieldset>
+                </form>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="3" align="center">Copyright &COPY 2021</td>
+        </tr>
+    </table>
+</body>
+</html>
