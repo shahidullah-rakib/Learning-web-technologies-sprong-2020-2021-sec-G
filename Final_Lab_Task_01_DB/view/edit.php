b4202e7259ae963ca5464bbcec360463b3539b37
shsidulla <57812923+shsidulla@users.noreply.github.com>
2021-03-21 11:58:38 +0600

    edit

--- Final_Lab_Task_01_DB/view/edit.php
+++ Final_Lab_Task_01_DB/view/edit.php
@@ -1,8 +1,11 @@
 <?php
+	require_once('../model/userModel.php');
 	$title = "Edit Page";
 	include('header.php');
+	$id=$_GET['id'];
+	$data=getUserbyId($id);
 
-	echo $_GET['id'];
+	$_SESSION['edit_id']=$id;
 ?>
 
 	<form method="post" action="../controller/update.php">
@@ -11,21 +14,21 @@
 			<table>
 				<tr>
 					<td>Username</td>
-					<td><input type="text" name="username" value="alamin"></td>
+					<td><input type="text" name="name" value="<?php echo$data['name'] ?>"></td>
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password" value="123"></td>
+					<td><input type="password" name="pass" value="<?php echo$data['password'] ?>"></td>
 				</tr>
 				
 				<tr>
 					<td>Email</td>
-					<td><input type="email" name="email" value="alamin@aiub.edu"></td>
+					<td><input type="email" name="email" value="<?php echo$data['email'] ?>"></td>
 				</tr>
 				<tr>
 					<td></td>
 					<td>
-						<input type="submit" name="signup" value="Update"> 
+						<input type="submit" name="submit" value="Update"> 
 						<a href="user_list.php">Back</a>
 					</td>
 				</tr>
