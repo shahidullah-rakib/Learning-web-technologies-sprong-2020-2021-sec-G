a39e9cb934a81ea4de2df4704fc1115f7fac5f3d
shsidulla <57812923+shsidulla@users.noreply.github.com>
2021-03-23 10:18:19 +0600

    update product

---
+++ FINAL_LAB_TASK_02_PHP/controller/update.php
@@ -0,0 +1,28 @@
+<?php 
+    require_once('../model/userModel.php');
+    session_start();
+    if(isset($_POST['submit'])){
+
+        $name=$_POST['productName'];
+        $bprice=$_POST['buyingPrice'];
+        $sprice=$_POST['sellingPrice'];
+
+        $id=$_SESSION['edit_id'];
+
+        $product = [	
+            'productName'	=> $productName, 
+            'buyingPrice'=> $buyingPrice, 
+            'sellingPrice'=>$sellingPrice, 
+            'id' => $id
+        ];
+        updateProduct($product);
+        //echo $id;
+        if(isset($_POST['display'])){
+            header('location: ../view/display.php');
+        }else{
+            
+        }
+       
+    }
+
+?>
